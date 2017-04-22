fd2a386deaba734dc0b0de9c413b40fc711c3f85
Tannus Esquerdo <tannusewrton@gmail.com>
2017-04-21 23:19:10 -0300

    Config Server

---
+++ app.js
@@ -0,0 +1,7 @@
+ {
+    "name": "Petfinder",
+    "description": "Where pets find their people",
+    "repository": "https://github.com/tannusesquerdo/petfinder-ionic-app",
+    "logo": "http://pereless360.com/img/SP/Ghost/360-logo-white.png",
+    "keywords": ["ionic2", "petfinder", "mobileApp"]
+  }
